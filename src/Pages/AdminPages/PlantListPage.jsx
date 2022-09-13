6e5ac537b0c283fe1b2893bb527114c6ea4576fb
jrkprogramming <jrkaibigan@gmail.com>
2022-09-12 23:53:29 -0500

    styled profile page

--- src/Pages/AdminPages/PlantListPage.jsx
+++ src/Pages/AdminPages/PlantListPage.jsx
@@ -48,22 +48,24 @@ const PlantListPage = () => {
   }
 
   return (
-    <div>
+    <div className="p-10 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mx-[30%] my-[5%]">
         
-        <h1>List of Plants</h1>
+        <h1 className="mt-0 mb-2 text-5xl font-medium leading-tight text-neutral-100">List of Plants</h1>
 
-        <button onClick={handleCreatePlant}>ADD PLANT TO LIST</button>
+        <button class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-green-500 dark:hover:bg-stone-400  dark:focus:ring-blue-800" onClick={handleCreatePlant}>ADD PLANT TO LIST</button>
 
         {errorDelete && <p>{errorDelete}</p>}
 
           {plants.map(plant => (
             <div>
-                  <p>{plant.id}</p>
-                  <p>{plant.name}</p>
-                  <p>{plant.price}</p>
-                  <p>{plant.description}</p>
-                  <Link to={`/admin/plant/${plant.id}/edit`}>EDIT</Link>
-                  <button onClick={() => handleDelete(plant.id)}>REMOVE PLANT</button>  
+                  <p className="mt-0 mb-2 text-2xl font-medium leading-tight text-neutral-100">PLANT #: {plant.id}</p>
+                  <p className="mt-0 mb-2 text-2xl font-medium leading-tight text-neutral-100">{plant.name}</p>
+                  <p className="mt-0 mb-2 text-2xl font-medium leading-tight text-neutral-100">{plant.price}</p>
+                  <p className="mt-0 mb-2 text-2xl font-medium leading-tight text-neutral-100">{plant.description}</p>
+                  <a class="inline-flex items-center py-2 px-3 mx-[3%] text-sm font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-green-500 dark:hover:bg-stone-400  dark:focus:ring-blue-800" href={`/admin/plant/${plant.id}/edit`}>EDIT</a>
+                  <button class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-green-500 dark:hover:bg-stone-400  dark:focus:ring-blue-800" onClick={() => handleDelete(plant.id)}>REMOVE PLANT</button>
+                  <br></br><br></br><br></br>
+
             </div>
 
             ))}
