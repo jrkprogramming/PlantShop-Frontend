7de27b6526499d873f2b3372181eb52ee6869357
jrkprogramming <jrkaibigan@gmail.com>
2022-09-16 19:30:04 -0500

    styled admin plant list view

--- src/Pages/AdminPages/PlantListPage.jsx
+++ src/Pages/AdminPages/PlantListPage.jsx
@@ -52,7 +52,7 @@ const PlantListPage = () => {
   return (
     <div className="p-10 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mx-[30%] my-[5%]">
         
-        {/* <h1 className="mt-0 mb-2 text-5xl font-medium leading-tight text-neutral-100">List of Plants</h1>
+        <h1 className="mt-0 mb-2 text-5xl font-medium leading-tight text-neutral-100">List of Plants</h1>
 
       <div className='flex flex-inline '>
         <button class="inline-flex items-center m-5 py-3 px-4 text-sm font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-green-500 dark:hover:bg-stone-400  dark:focus:ring-blue-800" onClick={handleCreatePlant}>ADD A PLANT</button>
@@ -61,7 +61,7 @@ const PlantListPage = () => {
       </div>
 
 
-        <br></br><br></br>
+        {/* <br></br><br></br>
 
           {plants.map(plant => (
             <div>
@@ -76,25 +76,30 @@ const PlantListPage = () => {
             </div>
 
             ))} */}
+{plants.map(plant => (
+<div class="max-w-sm w-100 lg:max-w-full lg:flex my-5">
+  <div  class="bg-[url(/what_a_rush.png')] h-100 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" title="Photo of Plant">
 
-<div class="max-w-sm w-full lg:max-w-full lg:flex">
-  <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" title="Woman holding a mug">
+    <img src={plant.image} class="h-100 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"></img>
 
   </div>
-  <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-gray rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+  <div class="w-[100%] h-100 border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-gray rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
     <div class="mb-8">
       <p class="text-sm text-gray-600 flex items-center">
-        Quantity: 
+      PLANT #: {plant.id}
       </p>
       <br></br>
-      <div class="text-white font-bold text-xl mb-2">Can coffee make you a better developer?</div>
-      <p class="text-white text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+      <div class="text-white font-bold text-xl mb-2">{plant.name}</div>
+      <p class="text-white text-base">Price: ${plant.price}</p>
+      <p class="text-white text-base">In Stock: {plant.quantity}</p>
+      <br></br>
+      <p class="text-white text-base">{plant.description}</p>
     </div>
-
+    <a class="inline-flex items-center py-2 px-3 mx-[3%] text-sm font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-green-500 dark:hover:bg-stone-400  dark:focus:ring-blue-800" href={`/admin/plants/${plant.id}/edit`}>EDIT</a>
 
   </div>
 </div>
-
+))}
     </div>
   )
 }
--- src/Pages/DeliveryMethodPage.jsx
+++ src/Pages/DeliveryMethodPage.jsx
@@ -16,10 +16,10 @@ const DeliveryMethodPage = () => {
 
     <h2 className="mt-0 mb-2 text-5xl font-medium leading-tight text-neutral-100">CHOOSE YOUR DELIVERY METHOD</h2>
 
-    <button type="button" onClick={handleDelivery} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-6 py-3.5 text-center dark:bg-stone-400 dark:hover:bg-stone-400 dark:focus:ring-blue-800" disabled>DELIVERY</button>
+    <button type="button" onClick={handleDelivery} class="justify-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-6 py-3.5 text-center dark:bg-stone-400 dark:hover:bg-stone-400 dark:focus:ring-blue-800" disabled>DELIVERY</button>
 
     <Link to='/payment'>
-    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-6 py-3.5 text-center dark:bg-green-500 dark:hover:bg-stone-400 dark:focus:ring-blue-800 m-10">PICKUP</button>
+    <button type="button" class="justify center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-6 py-3.5 text-center dark:bg-green-500 dark:hover:bg-stone-400 dark:focus:ring-blue-800 m-10">PICKUP</button>
     </Link>
 
 </div>
